73f170650a0cc038007f935fc1be679ca6480fb2
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-04-04 16:52:18 -0300

    crud_ok

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    #salvar os dados da tela para o banco
-    novo_usuario = Usuario()
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    novo_usuario.save()
-    #exibir todos os usuarios já cadastrados em uma nova pagina
-    usuarios ={
-        'usuarios':Usuario.objects.all()
-    
+    if request.method == 'POST':
+        # salvar os dados da tela para o banco
+        novo_usuario = Usuario()
+        novo_usuario.nome = request.POST.get('nome')
+        novo_usuario.idade = request.POST.get('idade')
+        novo_usuario.save()
+        # redirecionar após o salvamento
+        return redirect('listagem_usuarios')
+
+    # exibir todos os usuários já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
     }
-    
-    #retornar os dados a pagina de listagem de usuarios
+    # retornar os dados para a página de listagem de usuários
     return render(request, 'usuarios/usuarios.html', usuarios)
+
+def editar_usuario(request, id):
+    usuario = Usuario.objects.get(pk=id)
+    if request.method == 'POST':
+        usuario.nome = request.POST.get('nome')
+        usuario.idade = request.POST.get('idade')
+        usuario.save()
+        return redirect('listagem_usuarios')
+    return render(request, 'usuarios/editar_usuario.html', {'usuario': usuario})
+
+def excluir_usuario(request, id):
+    usuario = Usuario.objects.get(pk=id)
+    usuario.delete()
+    return redirect('listagem_usuarios')
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     # usuarios.com
     path('',views.home,name='home'),
     #usuarios.com/usuarios
-    path('usuarios/',views.usuarios,name='listagem_usuarios')
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
+    path('editar/<int:id>/', views.editar_usuario, name='editar_usuario'),
+    path('excluir/<int:id>/', views.excluir_usuario, name='excluir_usuario')
     
 ]
